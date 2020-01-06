8c6e96e615187a6d202ebfd7e871c560d4951ec5
552464955 <552464955@qq.com>
2020-01-06 23:21:10 +0800

    1

--- Python学习/单例模式.py
+++ Python学习/单例模式.py
@@ -1,13 +1,17 @@
 class A(object):
     instance = None
+    flag = False
     def __new__(cls, *args, **kwargs):
         #判断对象是否为空
         if cls.instance is None:
             cls.instance = super().__new__(cls)
         return cls.instance
 
+    #TODO 让初始化只执行一次
     def __init__(self):
-        pass
+        if not self.flag:
+            print("a")
+            self.flag = True
 
 a = A()
 print(a)
