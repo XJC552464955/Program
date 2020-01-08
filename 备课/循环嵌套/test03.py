bef4ef8479a99a54faa5aa49d6bacca7023e333e
552464955 <552464955@qq.com>
2020-01-09 01:32:36 +0800

    1

---
+++ 备课/循环嵌套/test03.py
@@ -0,0 +1,21 @@
+# #找出1到10的数，将这些数分别乘于1到100的所有奇数
+# for i in range(1,100,2):
+#     for j in range(1,11):
+#         print(j,"*",i,"=",i*j, end=" ")
+#     print()
+#
+
+for i in range(100):
+    continue
+    # if i == 3:
+    #     break
+else:
+    print("for循环正常执行完了!")
+
+#找到1到100的质数
+# for i in range(1,101):
+#     for j in range(2,i):
+#         if i%j == 0:
+#             break
+#     else:
+#         print(i,"是质数")
