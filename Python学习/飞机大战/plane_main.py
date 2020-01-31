f5aa78fcb0a7b0c05e1e5ffa541291c3f50f30b4
552464955 <552464955@qq.com>
2020-02-01 00:37:07 +0800

    1

--- Python学习/飞机大战/plane_main.py
+++ Python学习/飞机大战/plane_main.py
@@ -1,9 +1,6 @@
 import pygame
 from Python学习.飞机大战.plane_sprites import *
 
-#屏幕大小的常量
-SCREEN_RECT = pygame.Rect(0,0,480,700)
-
 class PlaneGame(object):
     '''飞机大战主游戏'''
     def __init__(self):
@@ -18,16 +15,31 @@ class PlaneGame(object):
         #3.调用私有方法，精灵和精灵组的创建
         self.__create_sprites()
 
+        #4.设置定时器时间-创建敌机
+        pygame.time.set_timer(CREAT_ENEMY_EVENT,1000)
+        pygame.time.set_timer(PLAYER_FIRE_EVENT, 500)
+
     def __create_sprites(self):
         '''精灵生成方法'''
-        pass
+        #创建背景精灵和精灵组
+        bg1 = BackGround()
+        bg2 = BackGround(True)
+        self.back_ground = pygame.sprite.Group(bg1,bg2)
+        #创建敌机精灵组
+        self.enemy_ground = pygame.sprite.Group()
+        #创建玩家和玩家精灵组
+        self.player = Player()
+        self.player_ground = pygame.sprite.Group(self.player)
+        #创建爆炸组
+        self.enemy_boom = pygame.sprite.Group()
+
 
     def start_game(self):
         '''游戏启动方法'''
         print("游戏开始...")
         while True:
             #1.设置刷新帧率
-            self.clock.tick(60)
+            self.clock.tick(FRAME_PER_SEC)
             #2.事件监听
             self.__event_handler()
             #3.碰撞检测
@@ -36,23 +48,72 @@ class PlaneGame(object):
             self.__update_sprites()
             #5.更新显示
             pygame.display.update()
-            pass
 
     def __event_handler(self):
         '''事件监听'''
         for event in pygame.event.get():
             #判断是否退出游戏
-            if event.type == pygame.quit():
+            if event.type == pygame.QUIT:
                 PlaneGame.__pygame_over()
+            elif event.type == CREAT_ENEMY_EVENT:
+                # print("敌机出场》》》")
+                self.enemy = Eneny()
+                self.enemy_ground.add(self.enemy)
+            elif event.type == PLAYER_FIRE_EVENT:
+                self.player.fire()
+            # elif event.type == pygame.KEYDOWN and event.type == pygame.K_RIGHT:
+            #     print("向右")
 
+        #使用键盘模块提供的方法
+        key_pressed = pygame.key.get_pressed()
+        #判断元组中对应的按键索引值
+        if key_pressed[pygame.K_RIGHT]:
+            # print("向右移动")
+            self.player.speed = 2
+        elif key_pressed[pygame.K_LEFT]:
+            self.player.speed = -2
+        else:
+            self.player.speed = 0
 
     def __check_collider(self):
-        '''碰撞'''
-        pass
+        #1.子弹摧毁敌机
+        for enemy in self.enemy_ground:
+            for player in self.player_ground:
+                for player_bullet in self.player.bullet_ground:
+                    if pygame.sprite.collide_mask(player_bullet, enemy):
+                        player_bullet.kill()
+                        enemy.isboom = True
+                        self.enemy_ground.remove(enemy)
+                        self.enemy_boom.add(enemy)
+
+
+
+        # '''碰撞'''
+        # #groupcollide检测两个精灵组的碰撞，第三个参数控制第一个精灵组是否删除，第四个参数控制第二个精灵组
+        # pygame.sprite.groupcollide(self.enemy_ground,self.player.bullet_ground,True,True)
+        #spritecollide检测某个精灵和精灵组的碰撞,第三个参数控制精灵组是否删除
+        enemies = pygame.sprite.spritecollide(self.player,self.enemy_ground,True)
+        if len(enemies) > 0:
+            self.player.kill()
+            PlaneGame.__pygame_over()
 
     def __update_sprites(self):
         '''更新精灵'''
-        pass
+        self.back_ground.update()
+        self.back_ground.draw(self.screen)
+
+        self.enemy_ground.update()
+        self.enemy_ground.draw(self.screen)
+
+        self.enemy_boom.update()
+        self.enemy_boom.draw(self.screen)
+
+        self.player_ground.update()
+        self.player_ground.draw(self.screen)
+
+        self.player.bullet_ground.update()
+        self.player.bullet_ground.draw(self.screen)
+
 
     @staticmethod   #设置静态方法
     def __pygame_over():
--- Python学习/飞机大战/plane_sprites.py
+++ Python学习/飞机大战/plane_sprites.py
@@ -1,5 +1,14 @@
+import random
 import pygame
 
+#屏幕大小的常量
+SCREEN_RECT = pygame.Rect(0,0,480,700)
+#刷新的帧率
+FRAME_PER_SEC = 60
+#创建敌机的定时常量
+CREAT_ENEMY_EVENT = pygame.USEREVENT
+#player子弹事件
+PLAYER_FIRE_EVENT = pygame.USEREVENT + 1
 
 class GameSprite(pygame.sprite.Sprite):
     '''飞机大战游戏精灵'''
@@ -9,10 +18,105 @@ class GameSprite(pygame.sprite.Sprite):
         super().__init__()
 
         #定义对象属性
-        self.image = pygame.image.load(image_name)
-        self.rect = self.image.get_rect()
-        self.speed = speed
+        try:
+            self.image = pygame.image.load(image_name)
+            self.rect = self.image.get_rect()
+            self.speed = speed
+        except:
+            print("没找到图片")
 
     def update(self):
         #在屏幕的垂直方向上移动
         self.rect.y += self.speed
+
+class BackGround(GameSprite):
+    '''游戏背景精灵'''
+    def __init__(self,is_alt = False):
+        #调用父类方法，完成精灵创建
+        super().__init__("./images/background.png")
+
+        if is_alt:
+            self.rect.y = -self.rect.height
+
+    def update(self):
+        #调用父类的方法实现
+        #T判断是否移除屏幕
+        super().update()
+        if self.rect.y >= SCREEN_RECT.height:
+            self.rect.y = -self.rect.height
+
+class Eneny(GameSprite):
+    def __init__(self, num=1):
+        self.num = num   #记录敌机图片
+        #1.调用父类方法创建敌机精灵,同时指定敌机图片
+        super().__init__("./images/enemy"+str(num)+".png")
+        #2.指定敌机的初始速度
+        self.speed = random.randrange(1,3)
+
+        #3.指定敌机的初始随机位置
+        self.rect.bottom = 0
+        self.rect.x = random.randint(10,SCREEN_RECT.right-self.rect.width//2-20)
+        self.enemy_boom = pygame.sprite.Group()
+
+        #4.爆炸效果
+        self.isboom = False
+        self.index = 1
+
+    def update(self):
+        #1. 调用父类方法，保持垂直方向飞行
+        super().update()
+
+        #2.判断是否飞出屏幕
+        if self.rect.y >= SCREEN_RECT.height:
+            # print("飞出了屏幕")
+            self.kill()
+
+        if self.isboom:  #爆炸控制
+            if self.index < 17:   #多执行几遍让爆炸显示出来
+                self.image = pygame.image.load(
+                    "./images/enemy" + str(self.num) + "_down" + str(self.index//4) + ".png")
+                # 整除4是减慢爆炸的速度，如果按照update的频率60hz就太快了
+                self.index+=1
+            else:
+                self.kill()
+
+    def __del__(self):
+        print("敌机销毁",self.rect)
+
+
+class Player(GameSprite):
+    def __init__(self):
+        #1.调用父类方法，设置image
+        super().__init__("./images/me1.png")
+        #2.设置初始位置
+        self.rect.centerx = SCREEN_RECT.centerx
+        self.rect.bottom = SCREEN_RECT.bottom-120
+
+        #创建子弹精灵组
+        self.bullet_ground = pygame.sprite.Group()
+
+    def update(self):
+        self.rect.x += self.speed
+        if self.rect.left < 0:
+            self.rect.x = 0
+        elif self.rect.right > SCREEN_RECT.right:
+            self.rect.right = SCREEN_RECT.right
+    def fire(self):
+        # print("发射子弹")
+        bullet = Player_Bullet()
+        bullet.rect.bottom = self.rect.top
+        bullet.rect.centerx = self.rect.centerx
+        self.bullet_ground.add(bullet)
+
+class Player_Bullet(GameSprite):
+    def __init__(self):
+        super().__init__("./images/bullet1.png",-2)
+
+    def update(self):
+        super().update()
+        if self.rect.bottom < 0:
+            self.kill()
+
+    def __del__(self):
+        # print("子弹销毁")
+        pass
